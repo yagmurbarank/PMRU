7369d2990db042c33633e5a41f73f776f22d622d
Emir Batuhan Payal <batuhanpayal@gmail.com>
2023-12-20 16:59:57 +0300

    Add files via upload

---
+++ PMRU.Persistance/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PMRU.Persistance
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistance(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
