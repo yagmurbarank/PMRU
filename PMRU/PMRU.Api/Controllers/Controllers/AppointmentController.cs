597990db1fc80dbd73ee33a6fb50873cd24af4b1
Emir Batuhan Payal <batuhanpayal@gmail.com>
2024-01-02 14:15:21 +0300

    controller

---
+++ PMRU/PMRU.Api/Controllers/Controllers/AppointmentController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PMRU.Application.Features.Appointments.Queires.GetAppointments;
+
+namespace PMRU.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public AppointmentController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAppointment()
+        {
+            var response = await mediator.Send(new GetAppointmentQueryRequest());
+
+
+            return Ok(response);
+        }
+    }
+}
