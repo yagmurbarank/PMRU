a8baac233159ec548a7c6c4a0160b9ba18a4503e
Musa OCAK <136326424+musaocak1@users.noreply.github.com>
2024-01-03 12:58:05 +0300

    EmployeeCQRS added

---
+++ PMRU/PMRU.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Core;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PMRU.Application.Features.Employees.Queries.GetEmployeesById;
+
+namespace PMRU.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public EmployeeController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployeesById(GetEmployeesByIdQueryRequest request)
+        {
+            var response = await mediator.Send(request);
+            return Ok(response);
+        }
+    }
+}
---
+++ PMRU/PMRU.Application/Features/Employees/Queries/GetEmployeesById/GetEmployeesByIdQueryRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMRU.Application.Features.Employees.Queries.GetEmployeesById
+{
+    public class GetEmployeesByIdQueryRequest : IRequest<GetEmployeesByIdQueryResponse>
+    {
+        public int EmployeeId { get; set; }
+    }
+}
