3007b4c6996e797a7b962d04b31d100f984786d8
Doğa Demirtürk <ddemirturk18@ku.edu.tr>
2024-01-03 10:32:36 +0300

    Availabilities CQRS added

---
+++ PMRU/PMRU.Application/Features/Availabilities/Queries/GetAvailabilities/GetAvailabilitiesQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using PMRU.Application.Features.Appointments.Queires.GetAppointments;
+using PMRU.Application.Interfaces.UnitOfWorks;
+using PMRU.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMRU.Application.Features.Availabilities.Queries.GetAvailabilities
+{
+    public class GetAvailabilitiesQueryHandler : IRequestHandler<GetAvailabilitiesQueryRequest, IList<GetAvailabilitiesQueryResponse>>
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public GetAvailabilitiesQueryHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<IList<GetAvailabilitiesQueryResponse>> Handle(GetAvailabilitiesQueryRequest request, CancellationToken cancellationToken)
+        {
+            var availabilities = await unitOfWork.GetReadRepository<Availability>().GetAllAsync();
+
+            List<GetAvailabilitiesQueryResponse> response = new();
+
+            foreach (var availability in availabilities)
+            {
+                response.Add(new GetAvailabilitiesQueryResponse
+                {
+                    DoctorID = availability.DoctorID,
+                    Day = availability.Day,
+                    StartTime = availability.StartTime,
+                    EndTime = availability.EndTime,
+                });
+            }
+
+            return response;
+        }
+    }
+}
---
+++ PMRU/PMRU.Application/Features/Availabilities/Queries/GetAvailabilities/GetAvailabilitiesQueryResponse.cs
@@ -0,0 +1,17 @@
+﻿using PMRU.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMRU.Application.Features.Availabilities.Queries.GetAvailabilities
+{
+    public class GetAvailabilitiesQueryResponse
+    {
+        public int DoctorID { get; set; }
+        public DayOfWeek Day { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+    }
+}
