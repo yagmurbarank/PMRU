5b352426b0e10492e42e04e265b39e04116ac7d9
batuhanpayal <batuhanpayal@gmail.com>
2024-01-03 13:32:05 +0300

    mapper yapısı kuruldu.

---
+++ PMRU/PMRU.Mapper/Registration.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using PMRU.Application.Interfaces.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMRU.Mapper
+{
+    public static class Registration
+    {
+        public static void AddCustomMapper(this IServiceCollection services)
+        {
+            services.AddSingleton<IMapper, AutoMapper.Mapper>();
+        }
+    }
+}
