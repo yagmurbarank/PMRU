c41ed2763daad07111e3865f874d6205a6109597
yagmurbrnk <yagmurbarankarakus@outlook.com>
2024-01-03 12:15:10 +0300

    Add Doctor CQRS files

---
+++ PMRU/PMRU.Application/Features/Doctors/Queries/GetDoctors/GetDoctorQueryRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using PMRU.Application.Features.Appointments.Queires.GetAppointments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMRU.Application.Features.Doctors.Queries.GetDoctors
+{
+    public class GetDoctorQueryRequest : IRequest<IList<GetDoctorQueryResponse>>
+    {
+    }
+}
---
+++ PMRU/PMRU.Application/Features/Doctors/Queries/GetDoctors/GetDoctorQueryResponse.cs
@@ -0,0 +1,17 @@
+﻿using PMRU.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMRU.Application.Features.Doctors.Queries.GetDoctors
+{
+    public class GetDoctorQueryResponse
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Location Location { get; set; }
+        public ICollection<Availability> Availabilities { get; set; }
+    }
+}
