926f5ec2df119407bef456adc7d450b1a7eee1e0
yagmurbrnk <yagmurbarankarakus@outlook.com>
2024-01-04 22:21:36 +0300

    Add DoctorDto

--- PMRU/PMRU.Application/Features/Doctors/Queries/GetDoctors/GetDoctorQueryHandler.cs
+++ PMRU/PMRU.Application/Features/Doctors/Queries/GetDoctors/GetDoctorQueryHandler.cs
@@ -1,5 +1,9 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PMRU.Application.DTOs;
 using PMRU.Application.Features.Appointments.Queires.GetAppointments;
+using PMRU.Application.Interfaces.AutoMapper;
 using PMRU.Application.Interfaces.UnitOfWorks;
 using PMRU.Domain.Entities;
 using System;
@@ -7,36 +11,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IMapper = PMRU.Application.Interfaces.AutoMapper.IMapper;
 
 namespace PMRU.Application.Features.Doctors.Queries.GetDoctors
 {
     public class GetDoctorQueryHandler : IRequestHandler<GetDoctorQueryRequest, IList<GetDoctorQueryResponse>>
     {
         private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
 
-        public GetDoctorQueryHandler(IUnitOfWork unitOfWork)
+        public GetDoctorQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
         {
             this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
         }
         public async Task<IList<GetDoctorQueryResponse>> Handle(GetDoctorQueryRequest request, CancellationToken cancellationToken)
         {
+            
+            var Availabilities = await unitOfWork.GetReadRepository<Availability>().GetAllAsync(include: x => x.Include(b => b.Doctor));
             var doctors = await unitOfWork.GetReadRepository<Doctor>().GetAllAsync();
+            var map = mapper.Map<GetDoctorQueryResponse, Doctor>(doctors);
 
-            List<GetDoctorQueryResponse> response = new();
 
-            foreach (var doctor in doctors)
-            {
-                response.Add(new GetDoctorQueryResponse
-                {
-                    Name = doctor.Name,
-                    Surname = doctor.Surname,
-                    Location = doctor.Location,
-                    Availabilities = doctor.Availabilities,
-       
-
-                });
-            }
-            return response;
+            return map;
         }
     }
 }
