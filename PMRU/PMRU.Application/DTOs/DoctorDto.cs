235d450cc1221c7b3cd68a4075305cff50d3d7e8
Doğa Demirtürk <ddemirturk18@ku.edu.tr>
2024-01-06 15:50:42 +0300

    Availabilities automapper added

---
+++ PMRU/PMRU.Application/DTOs/DoctorDto.cs
@@ -0,0 +1,16 @@
+﻿using PMRU.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMRU.Application.DTOs
+{
+    public class DoctorDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string RegistrationNumber { get; set; }
+    }
+}
--- PMRU/PMRU.Application/Features/Availabilities/Queries/GetAvailabilities/GetAvailabilitiesQueryHandler.cs
+++ PMRU/PMRU.Application/Features/Availabilities/Queries/GetAvailabilities/GetAvailabilitiesQueryHandler.cs
@@ -1,5 +1,8 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PMRU.Application.DTOs;
 using PMRU.Application.Features.Appointments.Queires.GetAppointments;
+using PMRU.Application.Interfaces.AutoMapper;
 using PMRU.Application.Interfaces.UnitOfWorks;
 using PMRU.Domain.Entities;
 using System;
@@ -13,30 +16,21 @@ namespace PMRU.Application.Features.Availabilities.Queries.GetAvailabilities
     public class GetAvailabilitiesQueryHandler : IRequestHandler<GetAvailabilitiesQueryRequest, IList<GetAvailabilitiesQueryResponse>>
     {
         private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
 
-        public GetAvailabilitiesQueryHandler(IUnitOfWork unitOfWork)
+        public GetAvailabilitiesQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
         {
             this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
         }
 
         public async Task<IList<GetAvailabilitiesQueryResponse>> Handle(GetAvailabilitiesQueryRequest request, CancellationToken cancellationToken)
         {
-            var availabilities = await unitOfWork.GetReadRepository<Availability>().GetAllAsync();
+            var availabilities = await unitOfWork.GetReadRepository<Availability>().GetAllAsync(include: x => x.Include(b => b.Doctor));
+            var doctor = mapper.Map<DoctorDto, Doctor>(new Doctor());
+            var map = mapper.Map<GetAvailabilitiesQueryResponse, Availability>(availabilities);
 
-            List<GetAvailabilitiesQueryResponse> response = new();
-
-            foreach (var availability in availabilities)
-            {
-                response.Add(new GetAvailabilitiesQueryResponse
-                {
-                    DoctorID = availability.DoctorID,
-                    Day = availability.Day,
-                    StartTime = availability.StartTime,
-                    EndTime = availability.EndTime,
-                });
-            }
-
-            return response;
+            return map;
         }
     }
 }
