34ea512a3d155e4317f821cd0b8a8335f8465319
Musa OCAK <136326424+musaocak1@users.noreply.github.com>
2024-01-09 15:23:58 +0300

    İşçi örnekleri attırıldı. token service tamamlandı ve swagger'da auth arayüzü tanımlandı.

--- PMRU/PMRU.Infrastructure/Tokens/TokenService.cs
+++ PMRU/PMRU.Infrastructure/Tokens/TokenService.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -41,11 +42,11 @@ namespace PMRU.Infrastructure.Tokens
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.Secret));
 
             var token = new JwtSecurityToken(
-                issuer:tokenSettings.Issuer,
-                audience:tokenSettings.Audience,
-                expires:DateTime.Now.AddMinutes(tokenSettings.TokenValidityInMinutes),
+                issuer: tokenSettings.Issuer,
+                audience: tokenSettings.Audience,
+                expires: DateTime.Now.AddMinutes(tokenSettings.TokenValidityInMinutes),
                 claims: claims,
-                signingCredentials : new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
                 );
 
             await userManager.AddClaimsAsync(user, claims);
@@ -55,12 +56,34 @@ namespace PMRU.Infrastructure.Tokens
 
         public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
+            var randomNumber = new byte[64];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
         }
 
-        public ClaimsPrincipal? GetPrincipalFromExpiredToken()
+        public ClaimsPrincipal? GetPrincipalFromExpiredToken(string? token)
         {
-            throw new NotImplementedException();
+            TokenValidationParameters tokenValidationParameters = new()
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.Secret)),
+                ValidateLifetime = false,
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+            if (securityToken is not JwtSecurityToken jwtSecurityToken 
+                || !jwtSecurityToken.Header.Alg.
+                Equals(SecurityAlgorithms.HmacSha256, 
+                StringComparison.InvariantCultureIgnoreCase))
+            
+                throw new SecurityTokenException("Token bulunamadı!");
+
+            return principal;
+            
         }
     }
 }
--- PMRU/PMRU.Persistence/Configurations/EmployeeConfiguration.cs
+++ PMRU/PMRU.Persistence/Configurations/EmployeeConfiguration.cs
@@ -75,6 +75,143 @@ namespace PMRU.Persistence.Configurations
                     IsDeleted = false,
                     CreatedDate = DateTime.Now,
                     DeletedDate = null
+                },
+                new Employee
+                {
+                    Id = 3,
+                    IdentityNumber = "5278149630",
+                    Name = "Mehmet",
+                    Surname = "Çelik",
+                    Phone = "(555) 534-5678",
+                    Email = "mehmet.celik@email.com",
+                    LocationID = 35,
+                    PositionID = 5,
+                    DepartmentID = 3,
+                    RegistrationNumber = "102",
+                    IsActive = true,
+                    IsDeleted = false,
+                    CreatedDate = DateTime.Now,
+                    DeletedDate = null
+                },
+
+                new Employee
+                {
+                    Id = 4,
+                    IdentityNumber = "5555456789",
+                    Name = "Fatma",
+                    Surname = "Arslan",
+                    Phone = "(555) 545-6789",
+                    Email = "fatma.arslan@email.com",
+                    LocationID = 71,
+                    PositionID = 6,
+                    DepartmentID = 4,
+                    RegistrationNumber = "103",
+                    IsActive = true,
+                    IsDeleted = false,
+                    CreatedDate = DateTime.Now,
+                    DeletedDate = null
+                },
+                new Employee
+                {
+                    Id = 5,
+                    IdentityNumber = "5555567890",
+                    Name = "Ali",
+                    Surname = "Demir",
+                    Phone = "(555) 556-7890",
+                    Email = "ali.demir@email.com",
+                    LocationID = 72,
+                    PositionID = 2,
+                    DepartmentID = 2,
+                    RegistrationNumber = "104",
+                    IsActive = true,
+                    IsDeleted = false,
+                    CreatedDate = DateTime.Now,
+                    DeletedDate = null
+                },
+                new Employee
+                {
+                    Id = 6,
+                    IdentityNumber = "5555678901",
+                    Name = "Zeynep",
+                    Surname = "Yıldız",
+                    Phone = "(555) 567-8901",
+                    Email = "zeynep.yildiz@email.com",
+                    LocationID = 35,
+                    PositionID = 14,
+                    DepartmentID = 5,
+                    RegistrationNumber = "105",
+                    IsActive = true,
+                    IsDeleted = false,
+                    CreatedDate = DateTime.Now,
+                    DeletedDate = null
+                },
+                new Employee
+                {
+                    Id = 7,
+                    IdentityNumber = "5555789012",
+                    Name = "Can",
+                    Surname = "Aksoy",
+                    Phone = "(555) 578-9012",
+                    Email = "can.aksoy@email.com",
+                    LocationID = 41,
+                    PositionID = 4,
+                    DepartmentID = 2,
+                    RegistrationNumber = "106",
+                    IsActive = true,
+                    IsDeleted = false,
+                    CreatedDate = DateTime.Now,
+                    DeletedDate = null
+                },
+                new Employee
+                {
+                    Id = 8,
+                    IdentityNumber = "5555890123",
+                    Name = "Elif",
+                    Surname = "Ersoy",
+                    Phone = "(555) 589-0123",
+                    Email = "elif.ersoy@email.com",
+                    LocationID = 34,
+                    PositionID = 8,
+                    DepartmentID = 6,
+                    RegistrationNumber = "107",
+                    IsActive = true,
+                    IsDeleted = false,
+                    CreatedDate = DateTime.Now,
+                    DeletedDate = null
+                },
+                new Employee
+                {
+                    Id = 9,
+                    IdentityNumber = "5555901234",
+                    Name = "Burak",
+                    Surname = "Taşkın",
+                    Phone = "(555) 590-1234",
+                    Email = "burak.taskin@email.com",
+                    LocationID = 72,
+                    PositionID = 7,
+                    DepartmentID = 4,
+                    RegistrationNumber = "108",
+                    IsActive = true,
+                    IsDeleted = false,
+                    CreatedDate = DateTime.Now,
+                    DeletedDate = null
+                },
+                new Employee
+                {
+                    Id = 10,
+                    IdentityNumber = "5556012345",
+                    Name = "Deniz",
+                    Surname = "Akın",
+                    Phone = "(555) 601-2345",
+                    Email = "deniz.akin@email.com",
+                    LocationID = 71,
+                    PositionID = 2,
+                    DepartmentID = 1,
+                    RegistrationNumber = "109",
+                    IsActive = true,
+                    IsDeleted = false,
+                    CreatedDate = DateTime.Now,
+                    DeletedDate = null
                 }
 
             );
