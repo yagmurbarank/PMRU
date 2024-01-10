a8f19523f65fd00ad3d0f1c1443e02a97fc68487
batuhanpayal <batuhanpayal@gmail.com>
2024-01-10 13:46:48 +0300

    appointment rules yazıldı

--- PMRU/PMRU.Application/Features/Appointments/Command/CreateAppointment/CreateAppointmentCommandHandler.cs
+++ PMRU/PMRU.Application/Features/Appointments/Command/CreateAppointment/CreateAppointmentCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using PMRU.Application.Features.Appointments.Rules;
 using PMRU.Application.Interfaces.UnitOfWorks;
 using PMRU.Domain.Entities;
 using System;
@@ -12,14 +13,25 @@ namespace PMRU.Application.Features.Appointments.Command.CreateAppointment
     public class CreateAppointmentCommandHandler : IRequestHandler<CreateAppointmentCommandRequest, Unit>
     {
         private readonly IUnitOfWork unitOfWork;
+        private readonly AppointmentRules appointmentRules;
 
-        public CreateAppointmentCommandHandler(IUnitOfWork unitOfWork)
+        public CreateAppointmentCommandHandler(IUnitOfWork unitOfWork, AppointmentRules appointmentRules)
         {
             this.unitOfWork = unitOfWork;
+            this.appointmentRules = appointmentRules;
         }
         public async Task<Unit> Handle(CreateAppointmentCommandRequest request, CancellationToken cancellationToken)
         {
             Appointment appointment = new(request.DoctorID, request.EmployeeID, request.AppointmentDate, request.AppointmentHour, request.Description);
+
+            IList<Appointment> appointments = await unitOfWork.GetReadRepository<Appointment>().GetAllAsync();
+            /*
+            if (appointments.Any(x => x.AppointmentHour == request.AppointmentHour))
+                throw new Exception("doktorun bu saatte randevusu bulunmakta");
+            */
+
+            await appointmentRules.DoctorCannotHaveAppointmentAtTheSameHour(appointments, request.DoctorID, request.AppointmentHour, request.AppointmentDate);
+            
 
             await unitOfWork.GetWriteRepository<Appointment>().CreateAsync(appointment);
             await unitOfWork.SaveAsync();
---
+++ PMRU/PMRU.Application/Features/Appointments/Exceptions/DoctorCannotHaveAppointmentAtTheSameHourException.cs
@@ -0,0 +1,17 @@
+﻿using PMRU.Application.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMRU.Application.Features.Appointments.Exceptions
+{
+    public class DoctorCannotHaveAppointmentAtTheSameHourException : BaseException
+    {
+        public DoctorCannotHaveAppointmentAtTheSameHourException() : base("Doktorun bu saatte zaten randevusu var!")
+        {
+            
+        }
+    }
+}
