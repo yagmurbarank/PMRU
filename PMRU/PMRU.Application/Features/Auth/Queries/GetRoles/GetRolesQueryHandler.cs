466b8081e3fa4cf8d579c38991ae26bfae8356c7
Doğa Demirtürk <ddemirturk18@ku.edu.tr>
2024-01-15 23:50:44 +0300

    get roles added

---
+++ PMRU/PMRU.Application/Features/Auth/Queries/GetRoles/GetRolesQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using PMRU.Application.Features.Availabilities.Queries.GetAvailabilities;
+using PMRU.Application.Interfaces.AutoMapper;
+using PMRU.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMRU.Application.Features.Auth.Queries.GetRoles
+{
+    public class GetRolesQueryHandler : IRequestHandler<GetRolesQueryRequest, IList<GetRolesQueryResponseDto>>
+    {
+        private readonly RoleManager<Role> roleManager;
+        private readonly IMapper mapper;
+
+        public GetRolesQueryHandler(RoleManager<Role> roleManager, IMapper mapper)
+        {
+            this.roleManager = roleManager;
+            this.mapper = mapper;
+        }
+
+        public async Task<IList<GetRolesQueryResponseDto>> Handle(GetRolesQueryRequest request, CancellationToken cancellationToken)
+        {
+            var roles = roleManager.Roles.ToList();
+            var map = mapper.Map<GetRolesQueryResponseDto, Role>(roles);
+            return map;
+        }
+    }
+}
---
+++ PMRU/PMRU.Application/Features/Auth/Queries/GetRoles/GetRolesQueryRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using PMRU.Application.Features.Availabilities.Queries.GetAvailabilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMRU.Application.Features.Auth.Queries.GetRoles
+{
+    public class GetRolesQueryRequest : IRequest<IList<GetRolesQueryResponseDto>>
+    {
+    }
+}
