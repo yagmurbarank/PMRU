7c3bff3316c8d2e246d87e523cab2ffb8aac6436
yagmurbrnk <yagmurbarankarakus@outlook.com>
2024-01-17 21:02:37 +0300

    Add DoctorAppointment

--- PMRU/PMRU.BlazorUI/Contracts/IAppointmentsService.cs
+++ PMRU/PMRU.BlazorUI/Contracts/IAppointmentsService.cs
@@ -6,9 +6,9 @@ namespace PMRU.BlazorUI.Contracts
     public interface IAppointmentService
     {
         Task<List<AppointmentVM>> GetAppointments();
-        Task<List<AppointmentVM>> GetAppoitmentsByDoctorId(int doctorId);
+        Task<List<AppointmentVM>> GetAppointmentsByDoctorId(int doctorId);
         Task<AppointmentVM> GetAppointmentByEmployeeId(int employeeId);
-        Task<List<AppointmentVM>> GetAppoitmentsByDate(DateOnly date);
+        Task<List<AppointmentVM>> GetAppointmentsByDate(DateOnly date);
 
         Task<Response<Guid>> CreateAppointment(CreateAppointmentVM appointmentVM);
         Task<Response<Guid>> DeleteAppointment(DeleteAppointmentVM appointmentVM);
---
+++ PMRU/PMRU.BlazorUI/Pages/DoctorAppointment.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using PMRU.BlazorUI.Contracts;
+using PMRU.BlazorUI.Models.Appointment;
+using PMRU.BlazorUI.Models.Doctor;
+
+
+namespace PMRU.BlazorUI.Pages
+{
+    public partial class DoctorAppointment
+    {
+        [Inject]
+        public IAppointmentService appointmentService { get; set; }
+        [Inject]
+        public IDoctorService doctorService { get; set; }
+
+        private List<AppointmentVM> appointments { get; set; }
+        public DoctorVM Doctor { get; private set; } = new DoctorVM();
+        private int doctorId;
+        protected override async Task OnInitializedAsync()
+        {
+           
+            appointments = await appointmentService.GetAppointmentsByDoctorId(doctorId);
+        }
+      
+
+        private async Task GetAppointmentsByDoctorId()
+        {
+            appointments = await appointmentService.GetAppointmentsByDoctorId(doctorId);
+        }
+    }
+}
