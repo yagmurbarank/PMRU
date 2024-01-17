f7e58205d777a022a908d0eb4eea9cfdfac57ec8
batuhanpayal <batuhanpayal@gmail.com>
2024-01-17 16:24:25 +0300

    systemadmin için doktor ekleme ve doktorları görüntüleme

---
+++ PMRU/PMRU.BlazorUI/Pages/Doctors/Create.razor.cs
@@ -0,0 +1,29 @@
+﻿using Blazored.Toast.Services;
+using Microsoft.AspNetCore.Components;
+using PMRU.BlazorUI.Contracts;
+using PMRU.BlazorUI.Models.Doctor;
+
+namespace PMRU.BlazorUI.Pages.Doctors
+{
+    public partial class Create
+    {
+        /*[Inject] NavigationManager _navManager { get; set; }*/
+        [Inject] IDoctorService _client { get; set; }
+        /*[Inject] IToastService toastService { get; set; }*/
+        public string Message { get; private set; }
+        CreateDoctorVM doctor { get; set; } = new CreateDoctorVM();
+
+        async Task Createdoctor()
+        {
+            var response = await _client.CreateDoctor(doctor);
+            /*if (response.Success)
+            {
+                toastService.ShowSuccess("Leave Type created Successfully");
+                toastService.ShowToast(ToastLevel.Info, "Test");
+                _navManager.NavigateTo("/doctors/");
+            }
+            Message = response.Message;*/
+        }
+
+    }
+}
---
+++ PMRU/PMRU.BlazorUI/Pages/Doctors/FormComponent.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using PMRU.BlazorUI.Models.Doctor;
+
+namespace PMRU.BlazorUI.Pages.Doctors
+{
+    public partial class FormComponent
+    {
+        [Parameter] public bool Disabled { get; set; } = false;
+        [Parameter] public CreateDoctorVM Doctor { get; set; }
+        [Parameter] public string ButtonText { get; set; } = "Save";
+        [Parameter] public EventCallback OnValidSubmit { get; set; }
+    }
+}
