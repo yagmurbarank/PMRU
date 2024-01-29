d9ff2315b5a6bacfcb7afd952c7896f1b8af8a24
Musa OCAK <136326424+musaocak1@users.noreply.github.com>
2024-01-29 19:37:27 +0300

    Localization Implemented

---
+++ PMRU/PMRU.BlazorUI/Localization/CultureWithName.cs
@@ -0,0 +1,15 @@
+﻿namespace PMRU.BlazorUI.Localization
+{
+    public record CultureWithName
+    {
+
+        public string Name { get; init; } = default;
+        public string Culture { get; init; } = default;
+
+        public CultureWithName(string name,string culture)
+        {
+            Name = name;
+            Culture = culture;
+        }
+    }
+}
